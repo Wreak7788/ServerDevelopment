16045895a40443c1b1dd3db4b6b58b9c521b769f
lijingxin <lijingxin02@163.com>
2022-11-10 17:39:15 +0800

    update chapter3 code

--- StdCode/stdThread.c
+++ StdCode/stdThread.c
@@ -6,7 +6,6 @@ void threadFunc1()
     while(1)
     {
         printf("Func1 Thread\n");
-        // Sleep(1000);
     }
 }
 
@@ -17,10 +16,23 @@ void threadFunc2(int a, int b)
     }
 }
 
+void threadFunc3()
+{
+  while(true)
+    printf("Func3 Thread\n");
+}
+
+void func()
+{
+  std::thread t(threadFunc3);
+  //使用detach将线程对象与线程分离，否则局部变量t在func结束后释放将导致程序崩溃
+  t.detach();
+}
 int main()
 {
     std::thread t1(threadFunc1);
     std::thread t2(threadFunc2, 1, 2);
+    func();
     while(1){}
     return 0;
 }
